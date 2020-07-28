d2e42b8a5db0ec8978167b2474237aa4c5100c1c
jorge doncel <jdoncel27@gmail.com>
2020-07-28 17:46:47 +0200

    docker

--- API/api.py
+++ API/api.py
@@ -10,6 +10,10 @@ import numpy as np
 import pandas as pd
 import pymongo
 import matplotlib.pyplot as plt
+import urllib.request
+from bs4 import BeautifulSoup
+from string import Template
+import re
 
 df=pd.read_csv('songs_clean.csv')
 app = Flask(__name__, template_folder='template')
@@ -53,7 +57,27 @@ def answer(name):
     artist= feeling_song.iloc[0]["track_artist"]
     album= feeling_song.iloc[0]["track_album_name"]
     playlist= feeling_song.sample(n=1).iloc[0]["playlist_name"]
-    return dumps(f'Today you are {senti[max(range(len(pred2)), key = lambda x: pred2[x])]}. I recommend the song {song} by {artist} from the album  {album}. You can find in the Spotify playlist call {playlist}')
+    final_feeling= senti[max(range(len(pred2)), key = lambda x: pred2[x])]
+
+    space=" "
+    query=(song + space + artist).replace(' ', '+')
+    url = f"https://www.youtube.com/results?search_query={query}" 
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    web=str(soup.find("body"))
+    web=re.search(r'(videoId\"):(\"\w*\-?\w*)', web).group(2)
+    web=re.sub(r'[^\w]', ' ', web).split()[0]
+
+
+    vidtemplate = Template("""
+      <h1>Today you are $feeling. I recommend the song $song by $artist from the album  $album. You can find in the Spotify playlist call $album or watching this youtube video.
+
+    <h1>
+      <iframe src="https://www.youtube.com/embed/${youtube_id}" width="853" height="480" frameborder="0" allowfullscreen></iframe>
+    """)
+
+    return vidtemplate.substitute(youtube_id=web, feeling= final_feeling, song=song, artist=artist, album=album, playlist=playlist)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", PORT, debug=True)
